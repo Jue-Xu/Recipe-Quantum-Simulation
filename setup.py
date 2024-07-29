f7dcdeae8701c646960be7cfbc5c893a3c35df35
Jue Xu <xujuexujue@hotmail.com>
2024-07-30 00:29:25 +0800

    version

--- setup.py
+++ setup.py
@@ -20,13 +20,13 @@ if "-" in package_version:
 
 v_strs = package_version.split(".")
 print(v_strs)
-package_version = ".".join(v_strs[:-1]) + '.' + str(int(v_strs[-1])+1)
+# package_version = ".".join(v_strs[:-1]) + '.' + str(int(v_strs[-1])+1)
 assert "-" not in package_version
 assert "." in package_version
 
-# assert os.path.isfile("quantum_simulation_recipe/version.py")
-# with open("quantum_simulation_recipe/VERSION", "w", encoding="utf-8") as fh:
-#     fh.write("%s\n" % package_version)
+assert os.path.isfile("quantum_simulation_recipe/version.py")
+with open("quantum_simulation_recipe/VERSION", "w", encoding="utf-8") as fh:
+    fh.write("%s\n" % package_version)
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
